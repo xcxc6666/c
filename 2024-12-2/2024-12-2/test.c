9e6b5c57b3a12c77e79595a916ca3513f3e1252e
kexuan Wu <3033585934@qq.com>
2024-12-03 16:07:50 +0800

    Add files via upload

---
+++ 2024-12-2/2024-12-2/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char* pc = NULL;
+//    short* ps = NULL;
+//    int* pi = NULL;
+//    double* pd = NULL;
+//    printf("%zu\n",sizeof(pc));
+//    printf("%zu\n", sizeof(ps));
+//    printf("%zu\n", sizeof(pi));
+//    printf("%zu\n", sizeof(pd));
+//
+//    return 0;
+//}
+
