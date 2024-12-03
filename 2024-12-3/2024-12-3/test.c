769ac864993ad7ff8387b211323b86b34b194ffc
kexuan Wu <3033585934@qq.com>
2024-12-03 23:48:32 +0800

    Add files via upload

---
+++ 2024-12-3/2024-12-3/test.c
@@ -0,0 +1,30 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;//&arr[0]
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*p = i;
+//		p++;
+//	} 
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	printf("%d", p);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
