c14031aff7f106f2ead411a02f3053720d14b5ab
kexuan Wu <3033585934@qq.com>
2024-12-16 23:35:01 +0800

    Add files via upload

---
+++ 2024-12-16/2024-12-16/test.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+// 	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
+/*#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 0 };
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	return 0;
+}		*/					
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
