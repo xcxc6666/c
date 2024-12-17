f3e7ea7d6b675134797c99ca9ebd0f32b0adbb46
kexuan Wu <3033585934@qq.com>
2024-12-17 22:22:23 +0800

    Add files via upload

---
+++ 2024-12-17/2024-12-17/test.c
@@ -0,0 +1,69 @@
+ #define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	while (scanf("%d %d %d", &a, &b, &c) == 3)
+//	{
+//		if ((a + b > c) && (a + c > b) && (b + c > a))
+//		{
+//			if (a == b && b == c)
+//			{
+//				printf("Equilateral triangle!\n");
+//			}
+//			else if (a == b || a == c || b == c)
+//			{
+//				printf("Isosceles triangle!\n");
+//			}
+//			else
+//			{
+//				printf("Ordinary triangle!\n");
+//			}
+//		}
+//		else
+//		{
+//			printf("Not a triangle\n");
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	while (1)
+//	{
+//		printf("÷£–„Â˚ «…µ±∆\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr[10001] = { 0 };
+//	gets(arr);
+//	int left = 0;
+//	int right = strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		char tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
